73df2e32edd5e6d230861574e04890d54c2cc808
Andy <anfernandez@fi.uba.ar>
2022-08-14 15:17:39 -0300

    removes sql script from gitignore

---
+++ drop_db.sql
@@ -0,0 +1,6 @@
+SELECT pg_terminate_backend(pg_stat_activity.pid)
+FROM pg_stat_activity
+WHERE pg_stat_activity.datname = 'calyx_challenge'
+AND pid <> pg_backend_pid();
+
+ drop database calyx_challenge;
